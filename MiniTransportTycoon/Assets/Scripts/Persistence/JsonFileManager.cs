fcfc4ec8be1966e2b682b87402b59cd0a6b35178
Farkas Bence <f.bence2004@outlook.hu>
2026-02-28 17:15:05 +0100

    JsonFileManager created

---
+++ MiniTransportTycoon/Assets/Scripts/Persistence/JsonFileManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class JsonFileManager : IFileManager
+{
+    public async Task<GameData> LoadAsync(Stream stream)
+    {
+        try
+        {
+            using StreamReader reader = new StreamReader(stream);
+            return JsonUtility.FromJson<GameData>(await reader.ReadToEndAsync());
+        }
+        catch
+        {
+            throw new InvalidDataContractException();
+        }
+
+    }
+
+    public async Task SaveAsync(Stream stream, GameData gameData)
+    {
+        try
+        {
+            await using StreamWriter writer = new StreamWriter(stream);
+            await writer.WriteLineAsync(JsonUtility.ToJson(gameData));
+        }
+        catch
+        {
+            throw new InvalidDataContractException();
+        }
+    }
+}
