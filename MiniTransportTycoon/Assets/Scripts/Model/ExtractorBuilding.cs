d9f075658e16345f55438b1e2589dc1f061e39a1
Horváth Olivér <d6018j@inf.elte.hu>
2026-02-28 18:09:44 +0100

    ExtractorBuilding class created

---
+++ MiniTransportTycoon/Assets/Scripts/Model/ExtractorBuilding.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class ExtractorBuilding : Facility
+{
+    public ExtractorBuilding(Resource prodRes, int maxCap, Location loc, 
+        float prodInterval = 10, Size size = null, 
+        bool destroyable = false, RateChangeHandler rch = null) 
+        : base(prodRes, maxCap, loc, prodInterval, size, destroyable, rch)
+    {
+    }
+
+    protected override void Produce(object sender, EventArgs e)
+    {
+        ResourceAmount = Math.Min(ResourceAmount + rch.GetValue(), maxCapacity);
+    }
+}
