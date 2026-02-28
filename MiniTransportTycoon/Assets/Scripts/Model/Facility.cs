8aee5ad9bd04b284d35cc1c4dddb8166f8cbc424
Horváth Olivér <d6018j@inf.elte.hu>
2026-02-28 18:09:29 +0100

    Facility class created

---
+++ MiniTransportTycoon/Assets/Scripts/Model/Facility.cs
@@ -0,0 +1,47 @@
+
+using System;
+
+public abstract class Facility : Cell, IAdvancable
+{
+    public Resource ProducedResource { get; private set; }
+    public int ResourceAmount { get; protected set; }
+    private Timer productionTimer;
+    protected int maxCapacity;
+    protected RateChangeHandler rch;
+
+    internal Facility(Resource prodRes, int maxCap, Location loc, float prodInterval = 10f, 
+        Size size = null, bool destroyable = false, RateChangeHandler rch = null) : base(loc, size, destroyable)
+    {
+        ProducedResource = prodRes;
+        maxCapacity = maxCap;
+        productionTimer = new Timer(prodInterval);
+        this.rch = rch ?? new RateChangeHandler();
+        
+        productionTimer.OnTimerEllapsed += Produce;
+    }
+
+    protected abstract void Produce(object sender, EventArgs e);
+
+    public void Tick(float deltaTime)
+    {
+        productionTimer.Tick(deltaTime);
+        rch.Tick(deltaTime);
+    }
+
+    public int GetProducedResource(int amount)
+    {
+        if (amount <= 0) return 0;
+
+        if (amount >= ResourceAmount)
+        {
+            int temp = ResourceAmount;
+            ResourceAmount = 0;
+            return temp;
+        }
+
+        ResourceAmount -= amount;
+        return amount;
+    }
+
+
+}
