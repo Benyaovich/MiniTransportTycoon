e2c987d5a06fafde0926f079c6de18a415766a31
Farkas Bence <f.bence2004@outlook.hu>
2026-02-28 19:03:37 +0100

    ForestCell created

---
+++ MiniTransportTycoon/Assets/Scripts/Model/ForestCell.cs
@@ -0,0 +1,43 @@
+using System;
+using JetBrains.Annotations;
+
+public class ForestCell : Cell, IAdvancable
+{
+
+    public int NumOfTrees { get; private set; } = 1;
+    private Timer growthTimer;
+    private Timer spreadTimer;
+    private readonly Random random;
+    [CanBeNull] public EventHandler<Location> OnSpread;
+    [CanBeNull] public EventHandler<Location> OnGrow;
+
+    public ForestCell(Location origin, Size size = null, bool destroyable = true,float growthInterval = 60, float spreadInterval = 60) : base(origin, size, destroyable)
+    {
+        growthTimer = new Timer(growthInterval);
+        growthTimer.OnTimerElapsed += GrowTree;
+        spreadTimer = new Timer(spreadInterval);
+        spreadTimer.OnTimerElapsed += Spread;
+        random = new Random();
+    }
+
+    public void Tick(float deltaTime)
+    {
+        growthTimer.Tick(deltaTime);
+        spreadTimer.Tick(deltaTime);
+    }
+
+    public void Spread([CanBeNull] object sender, EventArgs e)
+    {
+        if(!(NumOfTrees == 3 || NumOfTrees == 4)) return;
+        
+        bool spread = random.Next(3) == 1;
+        if (spread) OnSpread?.Invoke(this, Origin);
+    }
+
+    public void GrowTree([CanBeNull] object sender, EventArgs e)
+    {
+        if (NumOfTrees == 4) return;
+        NumOfTrees++;
+        OnGrow?.Invoke(this, Origin);
+    }
+}
