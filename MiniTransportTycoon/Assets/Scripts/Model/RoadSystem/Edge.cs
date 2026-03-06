31f543c5008f51f999c7ff7f1eb28396ec2c2473
Szelei Márk Álmos <exsmfm@inf.elte.hu>
2026-03-06 19:33:25 +0100

    Edge osztály hozzáadva

---
+++ MiniTransportTycoon/Assets/Scripts/Model/RoadSystem/Edge.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+
+public class Edge
+{
+    public Location A;
+    public Location B;
+    public int X; //azt gondolom a az x it a tav??
+
+    public Edge(Location a, Location b)
+    {
+        A = a;
+        B = b;
+
+        if (A.X == B.X)
+        {
+            if (A.Y == B.Y)
+            {
+                throw new ArgumentException("A két csúcs ugyanaz, így nem lehet út");
+            }
+
+            if (A.Y > B.Y)
+            {
+                X = A.Y -  B.Y;
+            }
+            else
+            {
+                X = B.Y - A.Y;
+            }
+        } else if (A.Y == B.Y)
+        {
+            if (A.X == B.X)
+            {
+                throw new ArgumentException("A két csúcs ugyanaz, így nem lehet út");
+            }
+            
+            if (A.X > B.X)
+            {
+                X = A.X - B.X;
+            }
+            else
+            {
+                X = B.X - A.X;
+            }
+        }
+        else
+        {
+            throw new ArgumentException("A ket csúcs nem egy vonalban van, így nem lehet út");
+        }
+    }
+}
