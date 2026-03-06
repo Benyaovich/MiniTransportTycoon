5714092963ef9956c95c671724db75a0c47691f1
Horváth Olivér <d6018j@inf.elte.hu>
2026-03-06 16:18:46 +0100

    Created road cell class

---
+++ MiniTransportTycoon/Assets/Scripts/Model/Cells/RoadCells/RoadCell.cs
@@ -0,0 +1,48 @@
+
+#nullable enable
+using System;
+using System.Collections.Generic;
+using Model.Enumerations;
+
+public class RoadCell : Cell, IPurchasable
+{
+    public bool IsVertexPoint { get; private set; }
+    public bool IsIntersection { get; private set; }
+    public TrafficLamp? Lamp { get; private set; }
+    public bool HasLamp => Lamp is not null;
+    public List<Direction> Directions { get; private set; }
+    public List<Vehicle> Vehicles { get; private set; }
+    public int BuildPrice { get; set; }
+
+
+    public RoadCell(Location origin, bool isIntersection, List<Direction> directions,
+        Size? size = null, bool destroyable = true)
+        : base(origin, size, destroyable)
+    {
+        IsIntersection = isIntersection;
+        Directions = directions;
+    }
+
+    public void AddVehicle(Vehicle vehicle)
+    {
+        if (Vehicles.Contains(vehicle)) return;
+        Vehicles.Add(vehicle);
+    }
+
+    public void RemoveVehicle(Vehicle vehicle)
+    {
+        if (!Vehicles.Contains(vehicle)) return;
+        Vehicles.Remove(vehicle);
+    }
+
+    public void AddLamp(TrafficLamp lamp)
+    {
+        Lamp = lamp;
+    }
+
+    public void Destroy()
+    {
+        // Gráfból ki kell szedni a csucsot meg hasonlok
+        throw new NotImplementedException();
+    }
+}
