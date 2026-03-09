2819917c673648796c54471fcd29bc1c8ae34c7f
Horváth Olivér <d6018j@inf.elte.hu>
2026-03-09 18:51:57 +0100

    created cell visual service class

---
+++ MiniTransportTycoon/Assets/Scripts/Controller/Building/CellVisualService.cs
@@ -0,0 +1,76 @@
+#nullable enable
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellVisualService
+{
+    private readonly Grid<GridObject> _grid;
+    private readonly Transform _parentTransform;
+    private readonly List<CellObjectTypeSO> _cellObjectTypeSos;
+
+    public CellVisualService(
+        Grid<GridObject> grid,
+        Transform parentTransform,
+        List<CellObjectTypeSO> cellObjectTypeSos)
+    {
+        _grid = grid;
+        _parentTransform = parentTransform;
+        _cellObjectTypeSos = cellObjectTypeSos;
+    }
+
+    public void CreateVisualForCell(Cell cell)
+    {
+        GridObject origin = _grid.GetGridObject(cell.Origin.X, cell.Origin.Y);
+        origin.SetVisual(InstantiateCellPrefab(origin));
+        LinkVisualToModel(origin);
+    }
+
+    public void DestroyVisualOnModelOrigin(GridObject go)
+    {
+        GridObject origin = _grid.GetGridObject(go.Model!.Origin.X, go.Model.Origin.Y);
+        if (origin.Visual is null) return;
+        UnityEngine.Object.Destroy(origin.Visual.gameObject);
+    }
+
+    private void LinkVisualToModel(GridObject origin)
+    {
+        if (origin.Model == null || origin.Visual == null)
+            return;
+
+        switch (origin.Model)
+        {
+            case Forest forest:
+                ForestVisual forestVisual = origin.Visual.GetComponent<ForestVisual>();
+                forestVisual.Setup(forest);
+                break;
+
+            case ProcessingBuildingSteel processingBuildingSteel:
+                break;
+        }
+    }
+
+    private Transform InstantiateCellPrefab(GridObject go)
+    {
+        CellObjectTypeSO cellObjectTypeSo = GetCellObjectTypeSoForCell(go.Model!);
+        return UnityEngine.Object.Instantiate(
+            cellObjectTypeSo.prefab,
+            go.Grid.GetWorldPosition(go.Location.X, go.Location.Y).UVXZ3(),
+            Quaternion.identity,
+            _parentTransform);
+    }
+
+    private CellObjectTypeSO GetCellObjectTypeSoForCell(Cell value)
+    {
+        string nameOfCell = value.GetType().Name;
+        CellObjectTypeSO co = _cellObjectTypeSos.Find(x => x.buildingType.ToString() == nameOfCell);
+
+        if (co is null)
+        {
+            Debug.LogError("CellObjectTypeSO doesnt exist.");
+            throw new NullReferenceException();
+        }
+
+        return co;
+    }
+}
