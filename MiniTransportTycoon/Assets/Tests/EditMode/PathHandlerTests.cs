4698f247c78c871cd6d7ae846ab6a110b2303f36
Farkas Bence <f.bence2004@outlook.hu>
2026-03-09 23:11:04 +0100

    pathhandler test update

--- MiniTransportTycoon/Assets/Tests/EditMode/PathHandlerTests.cs
+++ MiniTransportTycoon/Assets/Tests/EditMode/PathHandlerTests.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
+using UnityEngine;
 
 
 public class PathHandlerTests
@@ -89,6 +90,66 @@ public class PathHandlerTests
         Assert.IsFalse(pathHandler.ContainsVertex(testVertices[0]));
         pathHandler.AddVertex(testVertices[0]);
         Assert.IsTrue(pathHandler.ContainsVertex(testVertices[0]));
+    }
+
+    [Test]
+    public void GetPathFromRouteTest()
+    {
+        foreach (var item in testVertices)
+        {
+            pathHandler.AddVertex(item);
+        }
+        pathHandler.AddEdge(new Edge(testVertices[0], testVertices[1]));
+        pathHandler.AddEdge(new Edge(testVertices[0], testVertices[2]));
+        pathHandler.AddEdge(new Edge(testVertices[2], testVertices[3]));
+        pathHandler.AddEdge(new Edge(testVertices[3], testVertices[4]));
+        pathHandler.AddEdge(new Edge(testVertices[4], testVertices[5]));
+        
+        pathHandler.AddEdge(new Edge(testVertices[6], testVertices[7]));
+        pathHandler.AddEdge(new Edge(testVertices[6], testVertices[8]));
+        pathHandler.AddEdge(new Edge(testVertices[8], testVertices[9]));
+        pathHandler.AddEdge(new Edge(testVertices[9], testVertices[10]));
+        pathHandler.AddEdge(new Edge(testVertices[10], testVertices[11]));
+
+
+        List<Location> Route = new List<Location>()
+        {
+            new Location(0, 2),
+            new Location(3, 3),
+            new Location(0, 2)
+        };
+        foreach (var item in pathHandler.GetPathFromRoute(Route))
+        {
+            Debug.Log(item.X + ", " + item.Y);
+        }
+    }
+
+    [Test]
+    public void GetNeighborsTest()
+    {
+        foreach (var item in testVertices)
+        {
+            pathHandler.AddVertex(item);
+        }
+        pathHandler.AddEdge(new Edge(testVertices[0], testVertices[1]));
+        pathHandler.AddEdge(new Edge(testVertices[0], testVertices[2]));
+        pathHandler.AddEdge(new Edge(testVertices[2], testVertices[3]));
+        pathHandler.AddEdge(new Edge(testVertices[3], testVertices[4]));
+        pathHandler.AddEdge(new Edge(testVertices[4], testVertices[5]));
+        
+        pathHandler.AddEdge(new Edge(testVertices[6], testVertices[7]));
+        pathHandler.AddEdge(new Edge(testVertices[6], testVertices[8]));
+        pathHandler.AddEdge(new Edge(testVertices[8], testVertices[9]));
+        pathHandler.AddEdge(new Edge(testVertices[9], testVertices[10]));
+        pathHandler.AddEdge(new Edge(testVertices[10], testVertices[11]));
+        
+        Assert.AreEqual(2,pathHandler.GetNeighbours(testVertices[0]).Count);
+        Assert.IsTrue(pathHandler.GetNeighbours(testVertices[0]).Any(x=>x.Equals(testVertices[1])));
+        Assert.IsTrue(pathHandler.GetNeighbours(testVertices[0]).Any(x=>x.Equals(testVertices[2])));
+        
+        
+        Assert.AreEqual(1,pathHandler.GetNeighbours(new Location(0,2)).Count);
+        Assert.IsTrue(new Location(0,1).Equals(pathHandler.GetNeighbours(new Location(0,2)).First()));
     }
     
     [Test]
