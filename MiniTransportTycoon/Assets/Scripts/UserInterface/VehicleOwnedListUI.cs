44e33a33bd063b5ea3640ed5d5fba816756e6a3e
Horváth Olivér <d6018j@inf.elte.hu>
2026-03-29 14:32:53 +0200

    feature: owned vehicle list ui

---
+++ MiniTransportTycoon/Assets/Scripts/UserInterface/VehicleOwnedListUI.cs
@@ -0,0 +1,69 @@
+#nullable enable
+using System;
+using Controller.Vehicles;
+using ScriptableObjects.Vehicles;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UserInterface;
+
+public class VehicleOwnedListUI : MonoBehaviour
+{
+    [SerializeField] private UIDocument uiDocument = null!;
+    [SerializeField] private VehicleManager vehicleManager = null!;
+    [SerializeField] private VisualTreeAsset listItemTemplate = null!;
+    
+    private ScrollView _vehicleList = null!;
+    private VisualElement _infoPanel = null!;
+    
+
+    private void OnEnable()
+    {
+        VisualElement root = uiDocument.rootVisualElement;
+        _vehicleList = root.Q<ScrollView>("VehicleOwnedList");
+        _infoPanel = root.Q<VisualElement>("NoVehiclesOwnedInfoPanel");
+        _infoPanel.Q<Label>("Text").text = "You don't have any vehicles.";
+
+        vehicleManager.OnVehicleSold += HandleVehicleSold;
+        
+        RefreshList();
+    }
+
+    private void OnDisable()
+    {
+        vehicleManager.OnVehicleBought -= HandleVehicleSold;
+    }
+
+    private void HandleVehicleSold(object sender, EventArgs e)
+    {
+        RefreshList();
+    }
+
+    public void RefreshList()
+    {
+        InfoPanel();
+        _vehicleList.Clear();
+        foreach (Vehicle vehicle in vehicleManager.VehicleStorage.Vehicles)
+        {
+            VehicleSO? vehicleSo = vehicleManager.VehicleSos.Find(x => x.VehicleType == vehicle.GetType());
+            if(vehicleSo is null){ throw new NullReferenceException("Nincs ilyen VehicleSO"); }
+
+            VisualElement element = listItemTemplate.Instantiate();
+            element.Q<Label>("Name").text = vehicleSo.displayName;
+            element.Q<Label>("Capacity").text = "Capacity: " + vehicleSo.maxCarryCapacity;
+            element.Q<Label>("Speed").text = "Speed: " + Math.Round(1.0 / vehicleSo.speed * 150).ToString("0");
+            element.Q<Label>("Maintenance").text = "Maintenance: " + vehicleSo.maintenanceCost;
+        
+        
+            Button sellBtn = element.Q<Button>("SellBtn");
+            sellBtn.clicked += () => vehicleManager.SellVehicle(vehicle);
+            
+            _vehicleList.Add(element);
+        }
+    }
+
+    private void InfoPanel()
+    {
+        if (vehicleManager.VehicleStorage.Vehicles.Count == 0){ _infoPanel.Enable(); }
+        else{ _infoPanel.Disable(); }
+    }
+}
