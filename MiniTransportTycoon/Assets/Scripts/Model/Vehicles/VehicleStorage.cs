34d803169e2725723544444e67373ca485c73b5f
Horváth Olivér <d6018j@inf.elte.hu>
2026-03-29 14:28:39 +0200

    feature: vehicle storage

---
+++ MiniTransportTycoon/Assets/Scripts/Model/Vehicles/VehicleStorage.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Model.Interfaces;
+
+namespace Model.Vehicles
+{
+    public class VehicleStorage : IVehicleStorage
+    {
+        public event EventHandler<Vehicle> OnVehicleAdd; 
+        public event EventHandler<Vehicle> OnVehicleRemove;
+        public List<Vehicle> Vehicles { get; private set; } = new();
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            if (Vehicles.Contains(vehicle)) return;
+            Vehicles.Add(vehicle);
+            Vehicles = Vehicles.OrderByDescending(x => x.MoveSpeed).ToList();
+            OnVehicleAdd?.Invoke(this, vehicle);
+        }
+
+        public void RemoveVehicle(Vehicle vehicle)
+        {
+            if (!Vehicles.Contains(vehicle)) return;
+            Vehicles.Remove(vehicle);
+            OnVehicleRemove?.Invoke(this, vehicle);
+        }
+    }
+}
