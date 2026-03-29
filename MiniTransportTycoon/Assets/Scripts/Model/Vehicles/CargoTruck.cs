60ac8972b8ad28a8c88931efa7a351fd2883a887
Horváth Olivér <d6018j@inf.elte.hu>
2026-03-29 22:18:18 +0200

    feature: cargo trucks updated

--- MiniTransportTycoon/Assets/Scripts/Model/Vehicles/CargoTruck.cs
+++ MiniTransportTycoon/Assets/Scripts/Model/Vehicles/CargoTruck.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class CargoTruck : Vehicle
 {
-    public CargoTruck(Resource resource, float speed = 2, int maintenanceCost = 100, int purchaseCost = 1000, int resourceAmount = 50) 
-        : base(resource, speed, maintenanceCost, purchaseCost, resourceAmount) { }
+    public CargoTruck(Grid<ModelGridObject> grid, Resource resource, float speed = 2, int maintenanceCost = 100, int purchaseCost = 1000, int resourceAmount = 50) 
+        : base(grid, resource, speed, maintenanceCost, purchaseCost, resourceAmount) { }
 
-    protected override void LoadResource(Facility facility)
+    protected override void LoadResource(IResourceProvider resourceProvider)
     {
-        ResourceAmount = facility.GetProducedResource(MaxCapacity - ResourceAmount);
+        ResourceAmount += resourceProvider.GetResource(MaxCapacity - ResourceAmount);
     }
 
     protected override void UnloadResource(IDepositPoint depositPoint)
--- MiniTransportTycoon/Assets/Scripts/Model/Vehicles/CargoTrucks/IronCargoTruck.cs
+++ MiniTransportTycoon/Assets/Scripts/Model/Vehicles/CargoTrucks/IronCargoTruck.cs
@@ -2,9 +2,9 @@ namespace Model.Vehicles.CargoTrucks
 {
     public class IronCargoTruck : CargoTruck
     {
-        public IronCargoTruck(Resource resource = Resource.Iron, float speed = 2, int maintenanceCost = 100,
+        public IronCargoTruck(Grid<ModelGridObject> grid, Resource resource = Resource.Iron, float speed = 2, int maintenanceCost = 100,
             int purchaseCost = 1000, int resourceAmount = 50)
-            : base(resource, speed, maintenanceCost, purchaseCost, resourceAmount)
+            : base(grid, resource, speed, maintenanceCost, purchaseCost, resourceAmount)
         {
         }
     }
