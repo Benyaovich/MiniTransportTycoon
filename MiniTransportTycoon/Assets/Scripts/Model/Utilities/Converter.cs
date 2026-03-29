5b7dc9cdef7882cf79d3eed5bcfdc830fb6662e0
Horváth Olivér <d6018j@inf.elte.hu>
2026-03-29 22:18:03 +0200

    feature: converter class update

--- MiniTransportTycoon/Assets/Scripts/Model/Utilities/Converter.cs
+++ MiniTransportTycoon/Assets/Scripts/Model/Utilities/Converter.cs
@@ -64,20 +64,4 @@ public static class Converter
             _ => throw new Exception($"Opposite of {direction.ToString()} is not defined yet.")
         };
     }
-    
-    public static Direction ToDirection(this Location location, Location nextLocation)
-    {
-        int deltaX = nextLocation.X - location.X;
-        int deltaY = nextLocation.Y - location.Y;
-        
-        return (deltaX, deltaY) switch
-        {
-            (0, 1) => Direction.Up,
-            (0, -1) => Direction.Down,
-            (-1, 0) => Direction.Left,
-            (1, 0) => Direction.Right,
-            (0,0) => throw new ArgumentException("The two Locations are the same"),
-            _ => throw new Exception($"Can not convert this movement ({location.X},{location.Y} -> {nextLocation.X},{nextLocation.Y}) to direction.")
-        };
-    }
 }
