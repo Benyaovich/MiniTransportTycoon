e5a438f0d0e7097c8955112e0d505ed3c011de12
Horváth Olivér <d6018j@inf.elte.hu>
2026-03-29 14:33:20 +0200

    feature: UI utils class

---
+++ MiniTransportTycoon/Assets/Scripts/UserInterface/Utils.cs
@@ -0,0 +1,22 @@
+using UnityEngine.UIElements;
+
+namespace UserInterface
+{
+    public static class Utils
+    {
+        public static void ToggleVisibility(this VisualElement element)
+        { 
+            if (element.style.display == DisplayStyle.Flex){ Disable(element); }
+            else{ Enable(element); }
+        }
+
+        public static void Disable(this VisualElement element)
+        {
+            element.style.display = DisplayStyle.None;
+        }
+        public static void Enable(this VisualElement element)
+        {
+            element.style.display = DisplayStyle.Flex;
+        }
+    }
+}
