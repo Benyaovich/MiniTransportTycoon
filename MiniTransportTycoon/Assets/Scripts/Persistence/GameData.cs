764049e2dbed78ea4ae0ac22e2f03c6e27f5b778
Farkas Bence <f.bence2004@outlook.hu>
2026-03-30 16:27:18 +0200

    persistence: GameData updated

--- MiniTransportTycoon/Assets/Scripts/Persistence/GameData.cs
+++ MiniTransportTycoon/Assets/Scripts/Persistence/GameData.cs
@@ -1,4 +1,21 @@
+using System.Collections.Generic;
+
+[System.Serializable]
 public class GameData
 {
-    
+    public List<SerializableModelGridObject> GridArray = new();
+
+    public GameData(ModelGridObject[,] gridArray)
+    {
+        for (int y = 0; y < gridArray.GetLength(1); y++)
+        {
+            for (int x = 0; x < gridArray.GetLength(0); x++)
+            {
+                if (gridArray[x, y].Model != null)
+                {
+                    GridArray.Add(new SerializableModelGridObject(gridArray[x, y]));
+                }
+            }
+        }
+    }
 }
