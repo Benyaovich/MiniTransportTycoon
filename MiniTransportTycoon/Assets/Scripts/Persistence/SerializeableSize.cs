5ab8a262db26bfa99fdb52f5f3fb013bf1898a6a
Farkas Bence <f.bence2004@outlook.hu>
2026-03-30 16:20:44 +0200

    persistence: SerializableSize created

---
+++ MiniTransportTycoon/Assets/Scripts/Persistence/SerializeableSize.cs
@@ -0,0 +1,14 @@
+using System;
+
+[Serializable]
+public class SerializeableSize
+{
+    public int width;
+    public int height;
+        
+    public SerializeableSize(Size size)
+    {
+        width = size.Width;
+        height = size.Height;
+    }
+}
