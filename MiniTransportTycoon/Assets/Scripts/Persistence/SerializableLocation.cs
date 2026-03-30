398882792191b283888cb079a0ae1512b11c37fb
Farkas Bence <f.bence2004@outlook.hu>
2026-03-30 16:20:29 +0200

    persistence: SerializeableLocation created

---
+++ MiniTransportTycoon/Assets/Scripts/Persistence/SerializableLocation.cs
@@ -0,0 +1,14 @@
+using System;
+
+[Serializable]
+public class SerializableLocation
+{
+    public int x;
+    public int y;
+
+    public SerializableLocation(Location location)
+    {
+        x = location.X;
+        y = location.Y;
+    }
+}
