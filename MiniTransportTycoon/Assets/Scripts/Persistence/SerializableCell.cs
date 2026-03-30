4c2c762f78beef8225b1f2c81bbd02ac86f160a7
Farkas Bence <f.bence2004@outlook.hu>
2026-03-30 16:21:02 +0200

    persistence: SerializableCell created

---
+++ MiniTransportTycoon/Assets/Scripts/Persistence/SerializableCell.cs
@@ -0,0 +1,17 @@
+using System;
+using JetBrains.Annotations;
+
+[Serializable]
+public class SerializableCell
+{
+    [CanBeNull] public SerializableLocation origin;
+    public SerializeableSize size;
+    public bool destroyable;
+        
+    public SerializableCell(Cell cell)
+    {
+        origin = new SerializableLocation(cell.Origin);
+        size = new SerializeableSize(cell.Size);
+        destroyable = cell.Destroyable;
+    }
+}
