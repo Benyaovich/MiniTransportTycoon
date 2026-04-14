fa975991174d922b65532fef81402d4f34535222
d6018j <d6018j@inf.elte.hu>
2026-04-15 01:28:17 +0200

    feature: add functionality to buttons

--- MiniTransportTycoon/Assets/Scripts/UserInterface/GameUI/GameUI.cs
+++ MiniTransportTycoon/Assets/Scripts/UserInterface/GameUI/GameUI.cs
@@ -14,6 +14,12 @@ public class GameUI : MonoBehaviour
     private Button _ownedVehiclesBtn;
     private Button _selectRoadBtn;
     private Button _selectBusStopBtn;
+
+    private Button _selectedSpeed;
+    private Button _speedPauseBtn;
+    private Button _speed1Btn;
+    private Button _speed2Btn;
+    private Button _speed4Btn;
     
     private VisualElement _vehiclePurchasePanel;
     private VisualElement _vehicleOwnedPanel;
@@ -33,6 +39,12 @@ public class GameUI : MonoBehaviour
         _vehicleOwnedPanel = root.Q<VisualElement>("VehicleOwnedPanel");
         _buyVehiclesBtn.clicked += ToggleBuyVehicleListView;
         _ownedVehiclesBtn.clicked += ToggleOwnedVehicleListView;
+        _speedPauseBtn = root.Q<Button>("SpeedPauseBtn");
+        
+        _speed1Btn = root.Q<Button>("Speed1Btn");
+        _speed2Btn = root.Q<Button>("Speed2Btn");
+        _speed4Btn = root.Q<Button>("Speed4Btn");
+        _selectedSpeed = _speed1Btn;
 
         _vehiclePurchasePanel.Disable();
         _vehicleOwnedPanel.Disable();
@@ -42,7 +54,14 @@ public class GameUI : MonoBehaviour
 
         _selectBusStopBtn = root.Q<Button>("SelectBusStopBtn");
         _selectBusStopBtn.clicked += BuildSelectionManager.Instance.SelectBusStopObjectTypeSo;
+
+        _speedPauseBtn.clicked += SpeedPauseBtnOnClicked;
+        _speed1Btn.clicked += Speed1BtnOnClicked;
+        _speed2Btn.clicked += Speed2BtnOnClicked;
+        _speed4Btn.clicked += Speed4BtnOnClicked;
     }
+
+    
 
     private void OnDisable()
     {
@@ -50,6 +69,10 @@ public class GameUI : MonoBehaviour
         _ownedVehiclesBtn.clicked -= ToggleOwnedVehicleListView;
         _selectRoadBtn.clicked -= BuildSelectionManager.Instance.SelectDynamicRoadObjectTypeSo;
         _selectBusStopBtn.clicked -= BuildSelectionManager.Instance.SelectBusStopObjectTypeSo;
+        _speedPauseBtn.clicked -= SpeedPauseBtnOnClicked;
+        _speed1Btn.clicked -= Speed1BtnOnClicked;
+        _speed2Btn.clicked -= Speed2BtnOnClicked;
+        _speed4Btn.clicked -= Speed4BtnOnClicked;
     }
 
     private void ToggleOwnedVehicleListView()
@@ -68,7 +91,25 @@ public class GameUI : MonoBehaviour
     }
 
 
-    
+
+    private void SpeedPauseBtnOnClicked() => SetGameSpeed(0);
+    private void Speed1BtnOnClicked() => SetGameSpeed(1);
+    private void Speed2BtnOnClicked() => SetGameSpeed(2);
+    private void Speed4BtnOnClicked() => SetGameSpeed(4);
+
+    private void SetGameSpeed(int multiplier)
+    {
+        _selectedSpeed.RemoveFromClassList("selectedBtn");
+        
+        GameManager.Instance.SetGameSpeedMultiplier(multiplier);
+        
+        if (multiplier == 0){ _selectedSpeed = _speedPauseBtn; }
+        else if (multiplier == 1){ _selectedSpeed = _speed1Btn; }
+        else if (multiplier == 2){ _selectedSpeed = _speed2Btn; }
+        else if (multiplier == 4){ _selectedSpeed = _speed4Btn; }
+        
+        _selectedSpeed.AddToClassList("selectedBtn");
+    }
     
     
     
