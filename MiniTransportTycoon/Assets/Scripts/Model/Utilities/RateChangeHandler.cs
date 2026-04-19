e3cef0882590c1ff26e4d2f351f2883db0506f8d
d6018j <d6018j@inf.elte.hu>
2026-04-19 02:01:04 +0200

    refactor: update rate change handler

--- MiniTransportTycoon/Assets/Scripts/Model/Utilities/RateChangeHandler.cs
+++ MiniTransportTycoon/Assets/Scripts/Model/Utilities/RateChangeHandler.cs
@@ -2,59 +2,57 @@ using System;
 
 public class RateChangeHandler : IAdvancable
 {
-    private Timer rateChangeTimer;
-    private int minRate;
-    private int maxRate;
-    private int currentRate;
-    private int rateChange;
-
-    //rakd hozza osztalydiagramhoz
-    private float rateChangeInterval; //masodperc 
-    private System.Random rnd;
+    private readonly Timer _rateChangeTimer;
+    private readonly int _minRate;
+    private readonly int _maxRate;
+    public int CurrentRate { get; private set; }
+    private readonly int _rateChange;
+    public float RateChangeInterval { get; }
+    private readonly Random _rnd;
 
     public RateChangeHandler(int minRate = 0, int maxRate = 100, int rateChange = 10, float rateChangeInterval = 120, int currentRate = -1)
     {
-        rnd = new System.Random();
+        _rnd = new Random();
         
-        this.minRate = minRate;
-        this.maxRate = maxRate;
+        _minRate = minRate;
+        _maxRate = maxRate;
         
-        this.currentRate = currentRate == -1 ? rnd.Next(minRate + rateChange, maxRate - rateChange + 1) : currentRate;
+        CurrentRate = currentRate == -1 ? _rnd.Next(minRate + rateChange, maxRate - rateChange + 1) : currentRate;
 
-        if (this.currentRate < minRate || this.currentRate > maxRate)
+        if (CurrentRate < minRate || CurrentRate > maxRate)
         {
-            throw new ArgumentOutOfRangeException("currentRate", "Invalid current rate, must be between (minRate + rateChange) and (maxRate - rateChange)");
+            throw new ArgumentOutOfRangeException(nameof(currentRate), "Invalid current rate, must be between (minRate + rateChange) and (maxRate - rateChange)");
         }
-        this.rateChange = rateChange;
-        this.rateChangeInterval = rateChangeInterval;
+        _rateChange = rateChange;
+        RateChangeInterval = rateChangeInterval;
 
-        rateChangeTimer = new Timer(rateChangeInterval);
-        rateChangeTimer.OnTimerElapsed += (UpdateCurrentRate);
+        _rateChangeTimer = new Timer(rateChangeInterval);
+        _rateChangeTimer.OnTimerElapsed += (UpdateCurrentRate);
     }
     
 
     private void UpdateCurrentRate(object e, EventArgs args)
     {
-        int change = rnd.Next(0, 2);
+        int change = _rnd.Next(0, 2);
 
         //van itt egy padding igazabol a ket szel kozott
-        if (change == 0 && (currentRate - rateChange) >= (minRate + rateChange))
+        if (change == 0 && (CurrentRate - _rateChange) >= (_minRate + _rateChange))
         {
-            currentRate -= rateChange;
+            CurrentRate -= _rateChange;
         }  
-        else if (change == 1 && (currentRate + rateChange) <= (maxRate - rateChange))
+        else if (change == 1 && (CurrentRate + _rateChange) <= (_maxRate - _rateChange))
         {
-            currentRate += rateChange;
+            CurrentRate += _rateChange;
         }
     }
 
     public int GetValue()
     {
-        return rnd.Next(currentRate - rateChange, currentRate + rateChange + 1);
+        return _rnd.Next(CurrentRate - _rateChange, CurrentRate + _rateChange + 1);
     }
 
     public void Tick(float deltaTime)
     {
-        rateChangeTimer.Tick(deltaTime);
+        _rateChangeTimer.Tick(deltaTime);
     }
 }
