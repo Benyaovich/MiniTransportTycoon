2a173351bbae68fed9d6a86517b09a7d79e80b76
d6018j <d6018j@inf.elte.hu>
2026-04-20 22:03:23 +0200

    model: update forest class

--- MiniTransportTycoon/Assets/Scripts/Model/Cells/Forest.cs
+++ MiniTransportTycoon/Assets/Scripts/Model/Cells/Forest.cs
@@ -8,33 +8,33 @@ public class Forest : Cell, IAdvancable, IDestroyable
     public bool CanDestroy => true;
     public int DestroyPrice => NumOfTrees * 50;
     public int NumOfTrees { get; private set; }
-    private Timer growthTimer;
-    private Timer spreadTimer;
-    private readonly Random random;
+    private readonly Timer _growthTimer;
+    private readonly Timer _spreadTimer;
+    private readonly Random _random;
     [CanBeNull] public EventHandler<Location> OnSpread;
     [CanBeNull] public EventHandler<Location> OnGrow;
 
     public Forest(Location origin, Size size = null, bool destroyable = true,float growthInterval = 60, float spreadInterval = 60, int numOfTrees = 1) : base(origin, size, destroyable)
     {
-        growthTimer = new Timer(growthInterval);
-        growthTimer.OnTimerElapsed += GrowTree;
-        spreadTimer = new Timer(spreadInterval);
-        spreadTimer.OnTimerElapsed += Spread;
-        random = new Random();
+        _growthTimer = new Timer(growthInterval);
+        _growthTimer.OnTimerElapsed += GrowTree;
+        _spreadTimer = new Timer(spreadInterval);
+        _spreadTimer.OnTimerElapsed += Spread;
+        _random = new Random();
         NumOfTrees = numOfTrees;
     }
 
     public void Tick(float deltaTime)
     {
-        growthTimer.Tick(deltaTime);
-        spreadTimer.Tick(deltaTime);
+        _growthTimer.Tick(deltaTime);
+        _spreadTimer.Tick(deltaTime);
     }
 
     public void Spread([CanBeNull] object sender, EventArgs e)
     {
         if(!(NumOfTrees == 3 || NumOfTrees == 4)) return;
         
-        bool spread = random.Next(3) == 1;
+        bool spread = _random.Next(3) == 1;
         if (spread) OnSpread?.Invoke(this, Origin);
     }
 
