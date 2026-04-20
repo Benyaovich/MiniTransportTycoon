d1361acfd63cfb4656f0741ff8159cfd7844506e
Benyaovich <f.bence2004@outlook.hu>
2026-04-20 20:02:47 +0200

    refactor: forest ctor modified

--- MiniTransportTycoon/Assets/Scripts/Model/Cells/Forest.cs
+++ MiniTransportTycoon/Assets/Scripts/Model/Cells/Forest.cs
@@ -4,20 +4,21 @@ using JetBrains.Annotations;
 public class Forest : Cell, IAdvancable
 {
 
-    public int NumOfTrees { get; private set; } = 1;
+    public int NumOfTrees { get; private set; }
     private Timer growthTimer;
     private Timer spreadTimer;
     private readonly Random random;
     [CanBeNull] public EventHandler<Location> OnSpread;
     [CanBeNull] public EventHandler<Location> OnGrow;
 
-    public Forest(Location origin, Size size = null, bool destroyable = true,float growthInterval = 60, float spreadInterval = 60) : base(origin, size, destroyable)
+    public Forest(Location origin, Size size = null, bool destroyable = true,float growthInterval = 60, float spreadInterval = 60, int numOfTrees = 1) : base(origin, size, destroyable)
     {
         growthTimer = new Timer(growthInterval);
         growthTimer.OnTimerElapsed += GrowTree;
         spreadTimer = new Timer(spreadInterval);
         spreadTimer.OnTimerElapsed += Spread;
         random = new Random();
+        NumOfTrees = numOfTrees;
     }
 
     public void Tick(float deltaTime)
