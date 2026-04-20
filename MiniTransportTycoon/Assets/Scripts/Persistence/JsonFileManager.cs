a0dd06d8566ddde6b2b60c419eed6162d4b405cd
Benyaovich <f.bence2004@outlook.hu>
2026-04-20 20:13:00 +0200

    update: switch from jsonutility to newtonsoft json

--- MiniTransportTycoon/Assets/Scripts/Persistence/JsonFileManager.cs
+++ MiniTransportTycoon/Assets/Scripts/Persistence/JsonFileManager.cs
@@ -1,30 +1,35 @@
 using System.IO;
 using System.Runtime.Serialization;
 using System.Threading.Tasks;
-using UnityEngine;
+using Newtonsoft.Json;
 
 public class JsonFileManager : IFileManager
 {
+    private JsonSerializerSettings _jsonSettings = new JsonSerializerSettings()
+    {
+        TypeNameHandling = TypeNameHandling.Auto,
+        Formatting = Formatting.Indented
+    };
     public async Task<GameData> LoadAsync(string path)
     {
         try
         {
             using StreamReader reader = new StreamReader(path);
-            return JsonUtility.FromJson<GameData>(await reader.ReadToEndAsync());
+            return JsonConvert.DeserializeObject<GameData>(await reader.ReadToEndAsync(),_jsonSettings);
         }
         catch
         {
             throw new InvalidDataContractException();
         }
-
     }
 
     public async Task SaveAsync(string path, GameData gameData)
     {
         try
         {
+            string json = JsonConvert.SerializeObject(gameData, _jsonSettings);
             await using StreamWriter writer = new StreamWriter(path);
-            await writer.WriteLineAsync(JsonUtility.ToJson(gameData,true));
+            await writer.WriteLineAsync(json);
         }
         catch
         {
