0ac98609f1e1ba3ad6aef3e3634974bca98deeb0
Benyaovich <f.bence2004@outlook.hu>
2026-04-24 20:40:10 +0200

    update: ctor update

--- MiniTransportTycoon/Assets/Scripts/Model/Cells/Cities/SmallCity.cs
+++ MiniTransportTycoon/Assets/Scripts/Model/Cells/Cities/SmallCity.cs
@@ -1,7 +1,7 @@
 public class SmallCity : City
 {
-    public SmallCity(Location origin, Size size = null, bool destroyable = true, RateChangeHandler rch = null)
-        : base(origin, size, destroyable, rch)
+    public SmallCity(Location origin, Size size = null, bool destroyable = true, RateChangeHandler rch = null, int numOfPeople = 100)
+        : base(origin, size, destroyable, rch, numOfPeople)
     {
         Size = size ?? new Size(3, 3);
     }
