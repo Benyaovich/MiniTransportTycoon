f994d9e4830ef2da99d27c7f90f91ab77d99f332
Benyaovich <f.bence2004@outlook.hu>
2026-04-27 16:31:12 +0200

    update: ctor update

--- MiniTransportTycoon/Assets/Scripts/Model/Vehicles/SemiTrucks/WoodSemiTruck.cs
+++ MiniTransportTycoon/Assets/Scripts/Model/Vehicles/SemiTrucks/WoodSemiTruck.cs
@@ -3,8 +3,8 @@ namespace Model.Vehicles.SemiTrucks
     public class WoodSemiTruck : SemiTruck
     {
         public WoodSemiTruck(Grid<ModelGridObject> grid, Resource resource = Resource.Wood, float speed = 1, int maintenanceCost = 60,
-            int purchaseCost = 500, int maxCarryCapacity = 25)
-            : base(grid, resource, speed, maintenanceCost, purchaseCost, maxCarryCapacity)
+            int purchaseCost = 500, int maxCarryCapacity = 25,int resourceAmount = 0, Route? route = null, float maintenanceRemainingTime = 0, float? moveRemainingTime = null)
+            : base(grid, resource, speed, maintenanceCost, purchaseCost, maxCarryCapacity,resourceAmount: resourceAmount, route: route, maintenanceRemainingTime: maintenanceRemainingTime, moveRemainingTime: moveRemainingTime)
         {
         }
     }
