dacea3eba68cb2a163bab558ea1cb55ebbe20819
Benyaovich <f.bence2004@outlook.hu>
2026-04-27 16:31:47 +0200

    update: SVehicle updated

--- MiniTransportTycoon/Assets/Scripts/Persistence/Serializables/SVehicle.cs
+++ MiniTransportTycoon/Assets/Scripts/Persistence/Serializables/SVehicle.cs
@@ -1,11 +1,12 @@
 using System;
 using JetBrains.Annotations;
 using Model.Vehicles.Buses;
+using Model.Vehicles.CargoTrucks;
+using Model.Vehicles.SemiTrucks;
 
 [Serializable]
 public class SVehicle
 {
-    [CanBeNull] public SLocation currentLocation; // nem kell
     public int resourceAmount;
     [CanBeNull] public SRoute route;
     public float maintenanceRemainingTime;
@@ -13,16 +14,12 @@ public class SVehicle
     
     public SVehicle(Vehicle vehicle)
     {
-        if (vehicle.CurrentLocation != null)
-            currentLocation = new SLocation(vehicle.CurrentLocation);
         resourceAmount = vehicle.ResourceAmount;
         if (vehicle.Route != null)
             route = new SRoute(vehicle.Route);
         maintenanceRemainingTime = vehicle.MaintenanceTimer.RemainingTime;
         if (vehicle.MoveTimer != null)
             moveRemainingTime = vehicle.MoveTimer.RemainingTime;
-
-
     }
     
     public SVehicle() { }
@@ -34,3 +31,82 @@ public class SSlowBus : SVehicle
     public SSlowBus(SlowBus slowBus) : base(slowBus) { }
     public SSlowBus() { }
 }
+
+[Serializable]
+public class SFastBus : SVehicle
+{
+    public SFastBus(FastBus slowBus) : base(slowBus) { }
+    public SFastBus() { }
+}
+
+[Serializable]
+public class SCoalCargoTruck : SVehicle
+{
+    public SCoalCargoTruck(CoalCargoTruck coalCargoTruck) : base(coalCargoTruck) { }
+    public SCoalCargoTruck() { }
+}
+
+[Serializable]
+public class SIronCargoTruck : SVehicle
+{
+    public SIronCargoTruck(IronCargoTruck ironCargoTruck) : base(ironCargoTruck) { }
+    public SIronCargoTruck() { }
+}
+
+[Serializable]
+public class SPaperCargoTruck : SVehicle
+{
+    public SPaperCargoTruck(PaperCargoTruck paperCargoTruck) : base(paperCargoTruck) { }
+    public SPaperCargoTruck() { }
+}
+
+[Serializable]
+public class SSteelCargoTruck : SVehicle
+{
+    public SSteelCargoTruck(SteelCargoTruck steelCargoTruck) : base(steelCargoTruck) { }
+    public SSteelCargoTruck() { }
+}
+
+[Serializable]
+public class SWoodCargoTruck : SVehicle
+{
+    public SWoodCargoTruck(WoodCargoTruck woodCargoTruck) : base(woodCargoTruck) { }
+    public SWoodCargoTruck() { }
+}
+
+[Serializable]
+public class SCoalSemiTruck : SVehicle
+{
+    public SCoalSemiTruck(CoalSemiTruck coalSemiTruck) : base(coalSemiTruck) { }
+    public SCoalSemiTruck() { }
+}
+
+[Serializable]
+public class SIronSemiTruck : SVehicle
+{
+    public SIronSemiTruck(IronSemiTruck ironSemiTruck) : base(ironSemiTruck) { }
+    public SIronSemiTruck() { }
+}
+
+[Serializable]
+public class SPaperSemiTruck : SVehicle
+{
+    public SPaperSemiTruck(PaperSemiTruck paperSemiTruck) : base(paperSemiTruck) { }
+    public SPaperSemiTruck() { }
+}
+
+[Serializable]
+public class SSteelSemiTruck : SVehicle
+{
+    public SSteelSemiTruck(SteelSemiTruck steelSemiTruck) : base(steelSemiTruck) { }
+    public SSteelSemiTruck() { }
+}
+
+[Serializable]
+public class SWoodSemiTruck : SVehicle
+{
+    public SWoodSemiTruck(WoodSemiTruck woodSemiTruck) : base(woodSemiTruck) { }
+    public SWoodSemiTruck() { }
+}
+
+
