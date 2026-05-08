59f0d62803506cad2dc6ed3412edd999ee104b46
d6018j <d6018j@inf.elte.hu>
2026-05-08 23:08:32 +0200

    feature: add rotation to cell with setter

--- MiniTransportTycoon/Assets/Scripts/Model/Cells/Cell.cs
+++ MiniTransportTycoon/Assets/Scripts/Model/Cells/Cell.cs
@@ -6,12 +6,19 @@ public class Cell
     public Location Origin { get; private set; }
     public Size Size { get; protected set; }
     public bool Destroyable { get; private set; }
+    public int RotationDegrees { get; private set; }
     
     public Cell(Location origin, Size size = null, bool destroyable = true)
     {
         Origin = origin;
         Size = size ?? new Size(1, 1);
         Destroyable = destroyable;
+        RotationDegrees = 0;
+    }
+
+    public void SetRotation(int rotationDegrees)
+    {
+        RotationDegrees = rotationDegrees;
     }
     
     public List<Location> GetGridPositionList()
