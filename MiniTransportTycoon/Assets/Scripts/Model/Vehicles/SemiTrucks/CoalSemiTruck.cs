318f062717324762709753b0781dd8a0e49035c9
d6018j <d6018j@inf.elte.hu>
2026-05-08 20:22:09 +0200

    update: changed semi truck constructor values to match SO-s

--- MiniTransportTycoon/Assets/Scripts/Model/Vehicles/SemiTrucks/CoalSemiTruck.cs
+++ MiniTransportTycoon/Assets/Scripts/Model/Vehicles/SemiTrucks/CoalSemiTruck.cs
@@ -3,7 +3,7 @@ namespace Model.Vehicles.SemiTrucks
 {
     public class CoalSemiTruck : SemiTruck
     {
-        public CoalSemiTruck(Grid<ModelGridObject> grid, Resource resource = Resource.Coal, float speed = 1, int maintenanceCost = 50,
+        public CoalSemiTruck(Grid<ModelGridObject> grid, Resource resource = Resource.Coal, float speed = 4, int maintenanceCost = 50,
             int purchaseCost = 500, int maxCarryCapacity = 25,int resourceAmount = 0, Route? route = null, float maintenanceRemainingTime = 0, float? moveRemainingTime = null)
             : base(grid, resource, speed, maintenanceCost, purchaseCost, maxCarryCapacity,resourceAmount: resourceAmount, route: route, maintenanceRemainingTime: maintenanceRemainingTime, moveRemainingTime: moveRemainingTime)
         {
--- MiniTransportTycoon/Assets/Scripts/Model/Vehicles/SemiTrucks/SemiTruck.cs
+++ MiniTransportTycoon/Assets/Scripts/Model/Vehicles/SemiTrucks/SemiTruck.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class SemiTruck : Vehicle
 {
-    public SemiTruck(Grid<ModelGridObject> grid, Resource resource, float speed = 1, int maintenanceCost = 50, int purchaseCost = 500, int maxCarryCapacity = 25,int resourceAmount = 0, Route? route = null, float maintenanceRemainingTime = 0, float? moveRemainingTime = null) 
+    public SemiTruck(Grid<ModelGridObject> grid, Resource resource, float speed = 4, int maintenanceCost = 50, int purchaseCost = 500, int maxCarryCapacity = 25,int resourceAmount = 0, Route? route = null, float maintenanceRemainingTime = 0, float? moveRemainingTime = null) 
         : base(grid, resource, speed, maintenanceCost, purchaseCost, maxCarryCapacity,resourceAmount: resourceAmount, route: route, maintenanceRemainingTime: maintenanceRemainingTime, moveRemainingTime: moveRemainingTime) { }
 
     protected override void LoadResource(IResourceProvider resourceProvider)
