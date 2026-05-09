6e6138fc3c3038686b254ecd237eda0c1dfba01d
Szelei Márk Álmos <exsmfm@inf.elte.hu>
2026-05-09 18:27:13 +0200

    Refactor: Vehicle maintenence cost lowered
    Lowered by a 100% it was way too expensive

--- MiniTransportTycoon/Assets/Scripts/Model/Vehicles/Buses/FastBus.cs
+++ MiniTransportTycoon/Assets/Scripts/Model/Vehicles/Buses/FastBus.cs
@@ -3,7 +3,7 @@ namespace Model.Vehicles.Buses
 {
     public class FastBus : Bus
     {
-        public FastBus(Grid<ModelGridObject> grid, Resource resource = Resource.People, float speed = 2, int maintenanceCost = 100,
+        public FastBus(Grid<ModelGridObject> grid, Resource resource = Resource.People, float speed = 2, int maintenanceCost = 10,
             int price = 1000, int maxCarryCapacity = 50,int resourceAmount = 0, Route? route = null, float maintenanceRemainingTime = 100, float? moveRemainingTime = null, CityService? cityService = null)
             : base(grid, resource, speed, maintenanceCost, price, maxCarryCapacity,resourceAmount: resourceAmount, route: route, maintenanceRemainingTime: maintenanceRemainingTime, moveRemainingTime: moveRemainingTime, cityService: cityService)
         {
