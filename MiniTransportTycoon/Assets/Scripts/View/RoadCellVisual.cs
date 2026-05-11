8fc0eb098a266deee4c92e4408bf485002d72ed7
d6018j <d6018j@inf.elte.hu>
2026-05-11 17:30:39 +0200

    feature: add color changing to read cell visual

--- MiniTransportTycoon/Assets/Scripts/View/RoadCellVisual.cs
+++ MiniTransportTycoon/Assets/Scripts/View/RoadCellVisual.cs
@@ -1,11 +1,22 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class RoadCellVisual : MonoBehaviour
 {
+    private static readonly int BaseColorPropertyId = Shader.PropertyToID("_BaseColor");
+    private static readonly int ColorPropertyId = Shader.PropertyToID("_Color");
+
     private RoadCell _roadCell;
     [SerializeField] private GameObject highlightVisual;
+    private readonly List<Renderer> _highlightRenderers = new();
+    private MaterialPropertyBlock _propertyBlock = null!;
+
+    private void Awake()
+    {
+        _propertyBlock = new MaterialPropertyBlock();
+        _highlightRenderers.AddRange(highlightVisual.GetComponentsInChildren<Renderer>(true));
+    }
 
     public void Setup(RoadCell roadCell)
     {
@@ -27,6 +38,18 @@ public class RoadCellVisual : MonoBehaviour
 
     private void RoadCellOnHighlightEnabled(object sender, Location e)
     {
+        ApplyHighlightColor(_roadCell.HighlightColor);
         highlightVisual.SetActive(true);
+    }
+
+    private void ApplyHighlightColor(Color color)
+    {
+        foreach (Renderer highlightRenderer in _highlightRenderers)
+        {
+            highlightRenderer.GetPropertyBlock(_propertyBlock);
+            _propertyBlock.SetColor(BaseColorPropertyId, color);
+            _propertyBlock.SetColor(ColorPropertyId, color);
+            highlightRenderer.SetPropertyBlock(_propertyBlock);
+        }
     }
 }
