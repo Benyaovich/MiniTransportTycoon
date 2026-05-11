49ff76627f0adac74467c77d13af2393cb800bc5
d6018j <d6018j@inf.elte.hu>
2026-05-11 17:30:50 +0200

    feature: extend IHighlightable class

--- MiniTransportTycoon/Assets/Scripts/Model/Interfaces/IHighlightable.cs
+++ MiniTransportTycoon/Assets/Scripts/Model/Interfaces/IHighlightable.cs
@@ -1,12 +1,15 @@
 
 using System;
+using UnityEngine;
 
 public interface IHighlightable
 {
     public event EventHandler<Location> OnHighlightEnabled; 
     public event EventHandler<Location> OnHighlightDisabled; 
     public bool Highlighted { get; }
+    public Color HighlightColor { get; }
 
     public void SetHighlighted(bool value);
+    public void SetHighlightColor(Color color);
 
 }
